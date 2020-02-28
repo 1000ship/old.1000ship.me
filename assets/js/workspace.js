3970c3b311e44961838449fe7f38111a06e51cf3
1000ship <cjstjdgur1234@gmail.com>
2020-02-28 18:20:43 +0900

    fixed before masonry

---
+++ assets/js/workspace.js
@@ -0,0 +1,11 @@
+$('.workspace').masonry({
+    itemSelector: '.work',
+    columnWidth: '.work-sizer',
+    percentPosition: true
+});
+
+// $('.workspace').masonry({
+//     itemSelector: '.work',
+//     columnWidth: 160,
+//     gutter: 20,
+//   });
