2cef7b79ad04c609fe88fb564e20d40d531a061d
1000ship <cjstjdgur1234@gmail.com>
2020-05-20 17:25:54 +0900

    Modify CSS globalStyle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
 import Container from './Container'
-import { withRouter } from 'react-router-dom'
-import { F7Link } from 'framework7-react'
+import { withRouter, Link } from 'react-router-dom'
 
 const HeaderContainer = styled.div`
     background-color: var(--navbar-bg-color);
     backdrop-filter: blur(10px);
-    color: var(--navbar-text-color);
     padding: 13px;
     box-shadow: 0px 4px 4px var(--navbar-shadow-color);
     position: fixed;
     width: 100vw;
     z-index: 2;
-
+    color: var(--text-color-1);
 `
-const TitleText = styled.span`
+const TitleText = styled(Link)`
     font-weight: 900;
     font-size: 20px;
+    color: var(--text-color-1);
 `
 
 const Header = (props) => {
     return (
         <HeaderContainer>
             <Container>
-                <F7Link href="/somewhere">
-                    <TitleText>
-                        1000ship
-                    </TitleText>
-                </F7Link>
+                <TitleText href="/somewhere">
+                    1000ship
+                </TitleText>
             </Container>
         </HeaderContainer>
     )
