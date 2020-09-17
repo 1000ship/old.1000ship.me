f9633a43eb1d86a982d6711ebbe94a6c352dbcf0
1000ship <cjstjdgur1234@gmail.com>
2020-09-17 20:05:59 +0900

    style: 코드 정리

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Import Framework7
 import Framework7 from './framework7-custom.js';
-// Import Framework7-React Plugin
 import Framework7React from 'framework7-react';
-// Import Framework7 Styles
 import '../css/framework7-custom.less';
-// Import Icons and App Custom Styles
 import '../css/app.less';
 
-// Import App Component
 import App from '../components/app.jsx';
-
-// Init F7 React Plugin
 Framework7.use(Framework7React)
 
 import Header from '../components/Header.js';
